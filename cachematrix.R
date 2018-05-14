ae51160f0b75461c8df60206430cb252dfc6182b
liyao28 <leofrancisvaldez@outlook.com>
2018-05-14 03:34:59 -0400

    prj week 2 trial 1

--- cachematrix.R
+++ cachematrix.R
@@ -2,14 +2,44 @@
 ## functions do
 
 ## Write a short comment describing this function
+ #makeCacheMatrix function to
+ #set value of matrix
+ #get result value
+ #set inverse value
+ #get inverse value
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  inv <- NULL
+  set <- function(y) {
+    x <<- y
+    inv <<- NULL
+    
+  }
+  get <- function() x
+  setinverse <- function(inverse) inv <<- inverse
+  getinverse <- function() inv
+  list(set=set, get=get, setinverse=setinverse, getinverse=getinverse)
 }
 
 
 ## Write a short comment describing this function
+ #ret inv of matrix
+ #1 check if inverse has been computed
+ # if yes, show result, skip the computation
+ # if no, compute inverse and set value
+ # matrix is always invertible
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+  
+  inv <- x$getinverse()
+  if(!is.null(inv)) {
+    message("getting cached data.")
+    return(inv)
+  }
+  data <- x$get()
+  inv <- solve(data)
+  x$setinverse(inv)
+  inv
 }
+
